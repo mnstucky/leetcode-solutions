b3d5f2cc55e59b5127686f1c7ce6313d45808e48
Matt Stucky <mnstucky@gmail.com>
2020-05-23 06:56:33 -0500

    Solved Valid Parentheses problem

---
+++ src/leetCode/ValidParentheses.java
@@ -0,0 +1,32 @@
+package leetCode;
+
+// https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> testStack = new Stack<>();
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[')
+                testStack.push(c);
+            else {
+                switch (c) {
+                    case ')':
+                        if (testStack.empty() || testStack.pop() != '(') return false;
+                        break;
+                    case '}':
+                        if (testStack.empty() || testStack.pop() != '{') return false;
+                        break;
+                    case ']':
+                        if (testStack.empty() || testStack.pop() != '[') return false;
+                        break;
+                }
+            }
+        }
+        if (!testStack.empty()) return false;
+        return true;
+    }
+}
