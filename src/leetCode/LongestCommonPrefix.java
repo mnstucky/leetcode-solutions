925c792d52fe5e40843b11f295f6e11e77f54630
Matt Stucky <mnstucky@gmail.com>
2020-05-22 19:07:04 -0500

    Solved Longest Common Prefix and Palindrome Number problems

---
+++ src/leetCode/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package leetCode;
+
+// https://leetcode.com/problems/longest-common-prefix/
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        StringBuilder prefix = new StringBuilder(strs[0]);
+        for (int i = 1; i < strs.length; i++) {
+            if (prefix.length() == 0) return prefix.toString();
+            prefix.setLength(strs[i].length());
+            for (int j = 0; j < strs[i].length(); j++) {
+                if (strs[i].charAt(j) != prefix.charAt(j)) {
+                    prefix.setLength(j);
+                    break;
+                }
+            }
+        }        
+        return prefix.toString();
+    }
+}
---
+++ src/leetCode/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package leetCode;
+
+public class PalindromeNumber {
+	
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int length = (int) (Math.log10(x) + 1);
+        long leftModulo = (long) Math.pow(10, length);
+        int rightModulo = 10;
+        while (leftModulo > 10) {
+        	if (x % leftModulo / (leftModulo / 10) != x % rightModulo / (rightModulo / 10))
+                return false;
+        	leftModulo /= 10;
+        	rightModulo *= 10;
+        }
+        return true;
+    }
+}
