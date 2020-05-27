e333fc24721bbd681729430f0d13304ea5a96013
Matt Stucky <mnstucky@gmail.com>
2020-05-27 11:10:57 -0500

    Solved Count And Say

--- src/leetCode/CountAndSay.java
+++ src/leetCode/CountAndSay.java
@@ -7,11 +7,14 @@ public class CountAndSay {
         StringBuilder sb = new StringBuilder();
         int count = 1;
         for (int i = 0; i < s.length(); i++) {
-            if (i == s.length() - 1)
-                sb.append(count + s.charAt(i));
+            if (i == s.length() - 1) {
+                sb.append(count);
+                sb.append(s.charAt(i));
+            }
             else if (s.charAt(i + 1) == s.charAt(i)) count++;
             else {
-                sb.append(count + s.charAt(i));
+                sb.append(count);
+                sb.append(s.charAt(i));
                 count = 1;
             }
         }
@@ -19,7 +22,7 @@ public class CountAndSay {
     }
 
     public static void main(String[] args) {
-        System.out.println(countAndSay(1));
+//        System.out.println(countAndSay(1));
         System.out.println(countAndSay(2));
         System.out.println(countAndSay(3));
         System.out.println(countAndSay(4));
